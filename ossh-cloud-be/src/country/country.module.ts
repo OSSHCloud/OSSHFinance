252086f9dc4e2372d44bae7b12ed8997f1270a70
Muhammad Owais <rahneverd@gmail.com>
2025-01-03 10:49:20 +0500

    updated country.module

--- ossh-cloud-be/src/country/country.module.ts
+++ ossh-cloud-be/src/country/country.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CountryService } from './country.service';
 import { CountryController } from './country.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Country } from './entities/country.entity';
+import { CountryHistory } from './entities/country-history.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Country, CountryHistory])],
   controllers: [CountryController],
   providers: [CountryService],
 })
