24bed47cd48c480aacd8a4be57b41044ddb04435
Muhammad Owais <rahneverd@gmail.com>
2025-01-03 09:05:19 +0500

    added create endpoint in bank resource

--- ossh-cloud-be/src/bank/bank.controller.ts
+++ ossh-cloud-be/src/bank/bank.controller.ts
@@ -1,34 +1,115 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Request, Post, Body, UseGuards } from '@nestjs/common';
 import { BankService } from './bank.service';
-import { CreateBankDto } from './dto/create-bank.dto';
-import { UpdateBankDto } from './dto/update-bank.dto';
+import JwtAuthGuard from 'src/auth/jwt-auth.guard';
+import { CreateDto } from './dto/create.dto';
+import { addStandardParameters } from 'src/utils/commonFunctions';
+import { API_SUCCESS_MESSAGE, LID_CREATED_ID } from 'src/utils/constants';
+import { isEmpty } from 'lodash';
+import * as dayjs from 'dayjs';
+import { RestResponse } from 'src/utils/restResponse';
+import { FindAllDto } from './dto/find-all.dto';
 
 @Controller('bank')
 export class BankController {
-  constructor(private readonly bankService: BankService) {}
+  constructor(private readonly mainService: BankService) {}
 
-  @Post()
-  create(@Body() createBankDto: CreateBankDto) {
-    return this.bankService.create(createBankDto);
+  @UseGuards(JwtAuthGuard)
+  // @UseGuards(RoleGuard(Role.FullLovCategoryAccess, Role.AddLovCategory))
+  @Post('create')
+  async create(@Request() req: any, @Body() createDto: CreateDto) {
+    try {
+      const res = await Promise.all(
+        createDto?.data?.map((createPayload) => {
+          const standardParams = addStandardParameters(req.user, createPayload);
+          return this.mainService.create({
+            ...standardParams,
+            dmlStatus: LID_CREATED_ID,
+            dmlTimestamp: dayjs().format(),
+          });
+        }),
+      );
+
+      if (!isEmpty(res)) {
+        return RestResponse.success(res, API_SUCCESS_MESSAGE);
+      } else {
+        return RestResponse.notFound(res);
+      }
+    } catch (e) {
+      throw e;
+    }
   }
 
-  @Get()
-  findAll() {
-    return this.bankService.findAll();
-  }
+  // @UseGuards(JwtAuthGuard)
+  // // // @UseGuards(RoleGuard(Role.FullLovCategoryAccess, Role.FindAllLovCategory))
+  // @Post('findAll')
+  // async findAll(@Request() req: any, @Body() findAllDto: FindAllDto) {
+  //   try {
+  //     const res = await Promise.all(
+  //       findAllDto?.data?.map((findPayload) => {
+  //         const standardParams = addStandardParameters(req.user, findPayload);
+  //         return this.mainService.findAll(
+  //           standardParams,
+  //           findAllDto?.pagination,
+  //         );
+  //       }),
+  //     );
+  //     if (!isEmpty(res) && !isEmpty(res[0])) {
+  //       return RestResponse.success(res, API_SUCCESS_MESSAGE);
+  //     } else {
+  //       return RestResponse.notFound(res);
+  //     }
+  //   } catch (e) {
+  //     throw e;
+  //   }
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.bankService.findOne(+id);
-  }
+  // @UseGuards(JwtAuthGuard)
+  // // @UseGuards(RoleGuard(Role.FullLovCategoryAccess, Role.UpdateLovCategory))
+  // @Post('update')
+  // async update(@Request() req: any, @Body() updateDto: UpdateDto) {
+  //   try {
+  //     const res = await Promise.all(
+  //       updateDto?.data?.map((updatePayload) => {
+  //         const standardParams = addStandardParameters(req.user, updatePayload);
+  //         return this.mainService.update({
+  //           ...standardParams,
+  //           dmlStatus: LID_UPDATE_ID,
+  //           dmlTimestamp: dayjs().format(),
+  //         });
+  //       }),
+  //     );
+  //     if (!isEmpty(res)) {
+  //       return RestResponse.success(res, API_SUCCESS_MESSAGE);
+  //     } else {
+  //       return RestResponse.notFound(res);
+  //     }
+  //   } catch (e) {
+  //     throw e;
+  //   }
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBankDto: UpdateBankDto) {
-    return this.bankService.update(+id, updateBankDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.bankService.remove(+id);
-  }
+  // @UseGuards(JwtAuthGuard)
+  // // @UseGuards(RoleGuard(Role.FullLovCategoryAccess, Role.DeleteLovCategory))
+  // @Post('delete')
+  // async remove(@Request() req: any, @Body() deleteDto: DeleteDto) {
+  //   try {
+  //     const res = await Promise.all(
+  //       deleteDto.data.map((findPayload) => {
+  //         const standardParams = addStandardParameters(req.user, findPayload);
+  //         return this.mainService.delete({
+  //           ...standardParams,
+  //           dmlStatus: LID_DELETE_ID,
+  //           dmlTimestamp: dayjs().format(),
+  //         });
+  //       }),
+  //     );
+  //     if (!isEmpty(res)) {
+  //       return RestResponse.success(res, API_SUCCESS_MESSAGE);
+  //     } else {
+  //       return RestResponse.notFound(res);
+  //     }
+  //   } catch (e) {
+  //     throw e;
+  //   }
+  // }
 }
